cb0ce73148b8be6e651b9729c4dc90752393c33c
= <=>
2021-01-17 19:41:43 -0700

    nginx and production configurations for deploy

---
+++ config/settings/production.py
@@ -0,0 +1,16 @@
+from .base import *
+
+SECRET_KEY = 'c@4&!wy@@(m43-5@uo_&ablg%&tr9ve#!l2n0tyvv%$*c9(mhr'
+ALLOWED_HOSTS = ["*"]
+DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = "/static/"
